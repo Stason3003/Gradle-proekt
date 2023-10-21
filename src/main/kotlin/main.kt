13f20e5644f9f0bec3247d00c48dca8870f520b8
Stason <79644303003@yandex.ru>
2023-10-21 23:00:01 +1000

    New commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
-fun main(){
-    println("Hello Kotlin")
+fun main() {
+    val amound = 1_000_000
+    val komisia = amound * 0.75 / 100
+if (komisia > 35 )println("$komisia рублей")else println("35 рублей")
+
+
+
 }
+
+
+
+
+
+
+
