a53ca948aba50d5fccdc9ce00f5baab14499e642
Stason <79644303003@yandex.ru>
2023-10-25 00:18:54 +1000

    Add new committtе

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,47 +1,56 @@
 fun main() {
-    val amound = 1_000_000
-    val komisia = amound * 0.75 / 100
-    if (komisia > 35) println("$komisia рублей") else println("35 рублей")
-    val likes = 6011
-    val people2 = likes % 100
-    val people = likes % 10
-    if ( people == 1 && people2 == 1) println("Понравились $likes человеку") else println("Понравились $likes людям")
+    val timeSecond = 30000;
+    val result = agoToText(timeSecond)
+    println(result)
 
-    fun calculateDiscount(purchaseAmount: Int, isRegularCustomer: Boolean): Int {
-        var discount = 0
-        var totalAmount = purchaseAmount.toDouble()
+    val cardType = "MasterCard"
+    val previousTransfers = 200.0
+    val transferAmount = 500.0
 
-        if (totalAmount > 1000){
-            if (totalAmount <= 10000) {
-                discount = 100
-                println("Скидка 100 рублей")
-            } else{
-                    discount = (totalAmount * 0.05).toInt()
-                    println("Скидка 5% ")
-                }
-                totalAmount = totalAmount - discount.toDouble()
-        }
-
-        if (isRegularCustomer) {
-            val extraDiscount = ((totalAmount * 1) / 100).toInt()
-            discount = discount + extraDiscount
-            println("Постояному клиенту скидка 1%: $extraDiscount рублей")
-        }
-
-        return discount
-
-    }
-        val purchaseAmount = 6000
-        val isRegularCustomer = true
-        val discount = calculateDiscount(purchaseAmount, isRegularCustomer)
-        println("Общая сумма скидки: $discount рублей")
-
-
+    val commission = calculateCommission(cardType, previousTransfers, transferAmount)
+    println("Commission: $commission рублей")
 
 }
 
+fun agoToText(timeSecond: Int) = when {
+    timeSecond in 0..60 -> "Был(а) только что"
+    timeSecond in 61..(60 * 60) -> minutAgo(timeSecond / 60)
+    timeSecond in (60 * 60) + 1..(24 * 60 * 60) -> hourAgo(timeSecond / (60 * 60))
+    timeSecond in (24 * 60 * 60) + 1..(48 * 60 * 60) -> "Вчера"
+    timeSecond in (48 * 60 * 60) + 1..(72 * 60 * 60) -> "Позавчера"
+    else -> {
+        "Давно"
+    }
+}
 
+fun minutAgo(minut: Int) = when {
+    minut in 5..20 || minut % 10 in 5..9 || minut % 10 == 0 -> "$minut минут назат "
+    minut % 10 == 0 -> "$minut минуту назад"
+    minut % 10 in 2..4 -> "$minut минуты назад"
+    else -> ""
+}
 
+fun hourAgo(hour: Int) = when {
+    hour in 5..20 || hour % 10 in 5..9 || hour % 10 == 0 -> "$hour часов назад"
+    hour % 10 == 1 -> "$hour час назад"
+    hour % 10 in 2..4 -> "$hour часа назад"
+    else -> ""
+}
 
+fun calculateCommission(
+    cardType: String = "VK Pay",
+    previousTransfers: Double = 0.0,
+    transferAmount: Double
+): Double {
+    var commission = 0.0
+
+    if (cardType == "MasterCard" || cardType == "Maestro") {
+        if (previousTransfers + transferAmount > 300) {
+            commission = transferAmount * 0.6 / 100
+        }
+    }
+
+    return commission
+}
 
 
